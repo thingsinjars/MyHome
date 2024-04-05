ff4faabf2ebf8b8d4c8861b4e29a469cf1c97719
Simon Madine <147663+thingsinjars@users.noreply.github.com>
2024-04-05 09:25:58 +0100

    Update AuthenticationControllerTest.java

--- service/src/test/java/com/myhome/controllers/AuthenticationControllerTest.java
+++ service/src/test/java/com/myhome/controllers/AuthenticationControllerTest.java
@@ -36,13 +36,6 @@ public class AuthenticationControllerTest {
     MockitoAnnotations.initMocks(this);
   }
 
-  /**
-   * tests the login functionality of the `AuthenticationController`. It provides a
-   * default `LoginRequest`, `AuthenticationData`, and `HttpHeaders`, and asserts that
-   * the `ResponseEntity` status code is `OK`, the header size is 2, and the headers
-   * match the expected values. Additionally, it verifies that the `authenticationService.login()`
-   * method was called with the provided `LoginRequest`.
-   */
   @Test
   void loginSuccess() {
     // given
@@ -64,47 +57,11 @@ public class AuthenticationControllerTest {
     verify(authenticationService).login(loginRequest);
   }
 
-  /**
-   * creates a default login request with an email address of TEST_EMAIL and a password
-   * of TEST_PASSWORD.
-   * 
-   * @returns a `LoginRequest` object containing the email address "TEST_EMAIL" and
-   * password "TEST_PASSWORD".
-   */
   private LoginRequest getDefaultLoginRequest() {
     return new LoginRequest().email(TEST_EMAIL).password(TEST_PASSWORD);
   }
 
-  /**
-   * creates a new `AuthenticationData` instance with token `TOKEN` and test ID `TEST_ID`.
-   * 
-   * @returns an `AuthenticationData` object containing the token and test ID.
-   */
   private AuthenticationData getDefaultAuthenticationData() {
     return new AuthenticationData(TOKEN, TEST_ID);
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
