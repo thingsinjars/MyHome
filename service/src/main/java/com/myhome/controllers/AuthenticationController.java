c153987a884ccb70c96976826e09e2e461403b3d
Simon Madine <147663+thingsinjars@users.noreply.github.com>
2024-06-03 13:19:46 +0100

    Update AuthenticationController.java

--- service/src/main/java/com/myhome/controllers/AuthenticationController.java
+++ service/src/main/java/com/myhome/controllers/AuthenticationController.java
@@ -10,25 +10,20 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Is a Spring Boot REST controller that implements the AuthenticationApi interface.
- * It creates HTTP headers for logging in users and returns them in response entities.
- */
 @RequiredArgsConstructor
 @RestController
 public class AuthenticationController implements AuthenticationApi {
 
   private final AuthenticationService authenticationService;
 
-  /**
-   * Creates HTTP headers with user ID and JWT token for login purposes based on provided
-   * `AuthenticationData`.
-   * 
-   * @param authenticationData user details for creating HTTP headers.
-   * 
-   * @returns a set of HTTP headers containing the user ID and JWT token for authentication
-   * purposes.
-   */
+  @Override
+  public ResponseEntity<Void> login(@Valid LoginRequest loginRequest) {
+    final AuthenticationData authenticationData = authenticationService.login(loginRequest);
+    return ResponseEntity.ok()
+        .headers(createLoginHeaders(authenticationData))
+        .build();
+  }
+
   private HttpHeaders createLoginHeaders(AuthenticationData authenticationData) {
     final HttpHeaders httpHeaders = new HttpHeaders();
     httpHeaders.add("userId", authenticationData.getUserId());
